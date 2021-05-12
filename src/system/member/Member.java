4dda04e950344d92f0fd70e6b38f087618bd31bd
Michala Nybroe <michalanim@gmail.com>
2021-05-12 12:04:23 +0200

    beginning of Memberclass

--- src/system/member/Member.java
+++ src/system/member/Member.java
@@ -1,4 +1,23 @@
 package system.member;
 
+import java.util.Date;
+
 public class Member {
+    private String name;
+    private Date dateOfBirth;
+    private String email;
+    private String phoneNumber;
+    private boolean active;
+    private boolean notFlagged;
+    private double membershipPrice;
+
+    public Member(String name,
+                  Date dateOfBirth,
+                  String email,
+                  String phoneNumber,
+                  boolean active,
+                  boolean notFlagged,
+                  double membershipPrice) {
+
+    }
 }
