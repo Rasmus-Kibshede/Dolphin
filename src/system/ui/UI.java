729882087dee154360cd55d00d7754c7f514a8ae
Michala Nybroe <michalanim@gmail.com>
2021-05-12 15:37:14 +0200

    UI

--- src/system/ui/UI.java
+++ src/system/ui/UI.java
@@ -1,4 +1,23 @@
 package system.ui;
 
+import java.util.Scanner;
+
 public class UI {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void display(String message) {
+        System.out.println(message);
+    }
+
+    public void display(int message) {
+        System.out.println(message);
+    }
+
+    public int getInt() {
+        return scanner.nextInt();
+    }
+
+    public String getString() {
+        return scanner.nextLine();
+    }
 }
