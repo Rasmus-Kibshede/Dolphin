23adf8fe636cdadaff1d046deadb2c74f07da744
Michala Nybroe <michalanim@gmail.com>
2021-05-12 10:55:25 +0200

    beginning of program

--- src/system/Controller.java
+++ src/system/Controller.java
@@ -1,4 +1,13 @@
 package system;
 
 public class Controller {
+    public void run() {
+        //AdminMenu menu = new AdminMenu();
+        //menu(logIn);
+        
+    }
+
+    public String logIn() {
+        return "";
+    }
 }
--- src/system/menus/AdminMenu.java
+++ src/system/menus/AdminMenu.java
@@ -1,4 +1,8 @@
 package system.menus;
 
 public class AdminMenu {
+    private String logInType;
+    public AdminMenu(String logInType) {
+        this.logInType = logInType;
+    }
 }
