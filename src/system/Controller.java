e17a615c4767efb0002c39af74716b89f288f26d
Rasmus Kibshede <2740ralle@gmail.com>
2021-05-12 11:28:04 +0200

    Added toString in Menu and few lines in Contoller and AdminMenu

--- src/system/Controller.java
+++ src/system/Controller.java
@@ -1,13 +1,22 @@
 package system;
 
+import system.menus.AdminMenu;
+import system.menus.Menu;
+
 public class Controller {
     public void run() {
-        //AdminMenu menu = new AdminMenu();
-        //menu(logIn);
+        //AdminMenu adminMenu = new AdminMenu(logIn());
+
+        //Creates a menu that can display choices
+        //String[] menuChoices = {};
+        //Menu menu = new Menu("Menu", "Choose", menuChoices);
+
+        //adminMenu.startMenu();
         
     }
 
     public String logIn() {
+        //Checks the logIn file for a match
         return "";
     }
 }
--- src/system/menus/AdminMenu.java
+++ src/system/menus/AdminMenu.java
@@ -1,8 +1,25 @@
 package system.menus;
 
 public class AdminMenu {
+
     private String logInType;
+
     public AdminMenu(String logInType) {
         this.logInType = logInType;
     }
+
+    public void startMenu(){
+        switch (logInType){
+            case "chairman":
+                //call chairman menu
+                break;
+            case "cashier":
+                //call cashier menu
+                break;
+            case "coach":
+                //call coach menu
+                break;
+        }
+    }
+
 }
