0e3fbaa0199b4b7603498f9d805dc99cc28720d0
Kamille Nikolajsen <kamillenikolajsen@Kamilles-MacBook-Pro-3.local>
2021-05-13 18:27:18 +0200

    Added to CreateMember

--- src/system/MemberManager.java
+++ src/system/MemberManager.java
@@ -6,4 +6,8 @@ import java.util.ArrayList;
 
 public class MemberManager {
     private ArrayList<Member> members;
+
+    public ArrayList<Member> getMembers(){
+        return members;
+    }
 }
--- src/system/commands/CreateMember.java
+++ src/system/commands/CreateMember.java
@@ -1,14 +1,24 @@
 package system.commands;
 
 import system.MemberManager;
+import system.member.Member;
 import system.ui.UI;
+
+import java.time.LocalDate;
 
 public class CreateMember implements Command {
     private String name;
     private MemberManager memberManager;
 
     public CreateMember(MemberManager memberManager) {
+        String memberName = null;
+        LocalDate dateOfBirth = null;
+        String email = null;
+        String phoneNumber = null;
+        boolean active;
+        double memberShipPrice = 0;
 
+        memberManager.getMembers().add(new Member(memberName, dateOfBirth, email, phoneNumber, active, memberShipPrice));
     }
 
     @Override
