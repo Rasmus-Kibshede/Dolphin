4da3a38b68abf866bef8cd6503fca6ff29db752b
Kamille Nikolajsen <kamillenikolajsen@Kamilles-MacBook-Pro-3.local>
2021-05-16 13:45:31 +0200

    Added to Application

--- src/system/Application.java
+++ src/system/Application.java
@@ -17,36 +17,16 @@ public class Application {
        memberManager = new MemberManager();
        ui = new UI();
        command = new MainMenu(this);
-
     }
-
 
     public void start() {
         this.command.execute(this.ui);
-
-        /*
-        //AdminMenu adminMenu = new AdminMenu(logIn());
-
-        //Creates a menu that can display choices
-        //String[] menuChoices = {};
-        //Menu menu = new Menu("Menu", "Choose", menuChoices);
-
-        //adminMenu.startMenu();
-        Payment payment = new Payment();
-        LocalDate birth = LocalDate.parse("1997-06-18");
-        Member member = new Member("Michala",
-                birth,
-                "michalanim2gmail.com",
-                "50932812",
-                true,
-                payment.getPrice(birth, true));
-        System.out.println(member.getMembershipPrice());
-
-         */
     }
 
+    /*
     public String logIn() {
         //Checks the logIn file for a match
         return "";
     }
+     */
 }
