1b6fd5055eb398909d8e12f5965b2bc8c9e410aa
Rasmus Kibshede <2740ralle@gmail.com>
2021-05-16 19:34:56 +0200

    Completed execute method

--- src/system/commands/menus/MenuCommand.java
+++ src/system/commands/menus/MenuCommand.java
@@ -10,15 +10,22 @@ public abstract class MenuCommand implements Command {
   @Override
   public void execute(UI ui) {
     this.ui = ui;
+    Command[] commands = this.getCommands();
+
+    //Displays the current menu
     ui.displayLn(this.getName());
 
-    printMenu(this.getCommands());
+    //Displays the current command options
+    printMenu(commands);
+    ui.display("Choose your option: ");
 
-    int menuOption = this.chooseMenuNumber(this.getCommands());
+    int chosenMenuOption = this.chooseMenuNumber(commands);
 
+    //Gets the actual index, so it matches the command array indexing
+    final int MENU_OPTION_INDEX = chosenMenuOption-1;
 
-
-
+    Command commandOption = commands[MENU_OPTION_INDEX];
+    commandOption.execute(ui);
   }
 
   private void printMenu(Command[] commands) {
