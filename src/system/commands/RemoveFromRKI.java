818dae52eddff256e48ebf8d7f169bb9b4a13656
Rasmus Kibshede <2740ralle@gmail.com>
2021-05-18 12:01:47 +0200

    RemoveFromRKI done

---
+++ src/system/commands/RemoveFromRKI.java
@@ -0,0 +1,44 @@
+package system.commands;
+
+import system.MemberManager;
+import system.member.Member;
+import system.ui.UI;
+import java.util.ArrayList;
+
+public class RemoveFromRKI implements Command{
+
+  private MemberManager memberManager;
+
+  public RemoveFromRKI(MemberManager memberManager) {
+    this.memberManager = memberManager;
+  }
+
+  @Override
+  public void execute(UI ui) {
+
+    ArrayList<Member> members = memberManager.getMembersInRKI();
+
+    for (Member member: members) {
+      ui.displayLn(member.toString());
+    }
+
+    ui.displayLn("Removing person from RKI");
+
+    ui.display("Which person is to be removed? Please enter a number:");
+    int number = ui.getInt("Please enter an existing number");
+
+    for (int i = 0; i < members.size(); i++) {
+      Member member = members.get(i);
+      if (number == member.getMemberNumber()) {
+        members.remove(member);
+      }
+    }
+
+    memberManager.getFileHandler().saveFile(members, memberManager.getMEMBERS_IN_RKI());
+  }
+
+  @Override
+  public String getName() {
+    return "Remove from RKI";
+  }
+}
--- src/system/commands/menus/EconomyAdministration.java
+++ src/system/commands/menus/EconomyAdministration.java
@@ -5,26 +5,26 @@ import system.commands.*;
 
 //Kamille
 public class EconomyAdministration extends MenuCommand {
-    private Application application;
+  private Application application;
 
-    public EconomyAdministration(Application application) {
-        this.application = application;
-    }
+  public EconomyAdministration(Application application) {
+    this.application = application;
+  }
 
-    @Override
-    public String getName() {
-        return "Economy Administration";
-    }
+  @Override
+  public String getName() {
+    return "Economy Administration";
+  }
 
-    @Override
-    protected Command[] getCommands() {
-        return new Command[]{
-                new ExpectedIncome(this.application.getMemberManager()),
-                new ShowMembersInRKI(this.application.getMemberManager()),
-                new EditMemberInDebt(this.application.getMemberManager()),
-                new MoveMemberToRKI(this.application.getMemberManager())
+  @Override
+  protected Command[] getCommands() {
+    return new Command[]{
+        new ExpectedIncome(this.application.getMemberManager()),
+        new ShowMembersInRKI(this.application.getMemberManager()),
+        new EditMemberInDebt(this.application.getMemberManager()),
+        new MoveMemberToRKI(this.application.getMemberManager()),
+        new RemoveFromRKI(this.application.getMemberManager())
+    };
 
-        };
-
-    }
+  }
 }
