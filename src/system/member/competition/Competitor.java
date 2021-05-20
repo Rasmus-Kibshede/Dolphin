58cffc90e01d01be4404e8a541bd4711dc71d51e
Rasmus Kibshede <2740ralle@gmail.com>
2021-05-20 11:07:37 +0200

    Added training score to competitor

--- src/system/member/competition/Competitor.java
+++ src/system/member/competition/Competitor.java
@@ -8,8 +8,9 @@ public class Competitor extends Member {
 
     private TrainingScore trainingScore;
 
-    public Competitor(String name, LocalDate dateOfBirth, String email, String phoneNumber, boolean active, int memberNumber) {
+    public Competitor(String name, LocalDate dateOfBirth, String email, String phoneNumber, boolean active, int memberNumber, TrainingScore trainingScore) {
         super(name, dateOfBirth, email, phoneNumber, active, memberNumber);
+        setTrainingScore(null);
     }
 
     public void setTrainingScore(TrainingScore trainingScore) {
