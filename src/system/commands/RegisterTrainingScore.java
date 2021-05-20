5cf898b6ac66cc7bf1c81d191f072b2b44897c6d
Rasmus Kibshede <2740ralle@gmail.com>
2021-05-20 13:47:56 +0200

    Created Commands

---
+++ src/system/commands/RegisterTrainingScore.java
@@ -0,0 +1,15 @@
+package system.commands;
+
+import system.ui.UI;
+
+public class RegisterTrainingScore implements Command{
+  @Override
+  public void execute(UI ui) {
+
+  }
+
+  @Override
+  public String getName() {
+    return "Register Training Score";
+  }
+}
