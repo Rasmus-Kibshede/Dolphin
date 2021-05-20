47adce9375006c6f88ec7c1416a6646a2966ec24
Kamille Nikolajsen <knikolajsen4@gmail.com>
2021-05-20 18:11:14 +0200

    Securing merge

--- src/system/commands/menus/CompetitiveAdministration.java
+++ src/system/commands/menus/CompetitiveAdministration.java
@@ -19,32 +19,14 @@ public class CompetitiveAdministration extends MenuCommand{
   protected Command[] getCommands() {
 
     return new Command[]{
-<<<<<<< HEAD
         new ShowTopFive(),/*
         new ShowCompetitors(this.application.getMemberManager()),
-=======
-
-        //new ShowTopFive(this.application.getMemberManager()),
-        //new ShowCompetitors(this.application.getMemberManager()),
-        //new ShowTeams(this.application.getMemberManager(), this.application.getCompetitorManager()),
-        //new RegisterTrainingScore(this.application.getMemberManager()),
-        //new RegisterCompetition(this.application.getMemberManager(), this.application.getCompetitorManager()),
-        //new EditTeam(this.application.getMemberManager(), this.application.getCompetitorManager()),
-        //new EditMemberType()// Eller havde vi droppet den?
-
-        new ShowTopFive(),
-        new ShowCompetitors(this.application.getMemberManager()),/*
->>>>>>> 3b3a6106d668a1c48064ee18a2430a8f5ebbba26
         new ShowTeams(this.application.getMemberManager(), this.application.getCompetitorManager()),
         new RegisterTrainingScore(this.application.getMemberManager()),
         new RegisterCompetition(this.application.getMemberManager(), this.application.getCompetitorManager()),
         new EditTeam(this.application.getMemberManager(), this.application.getCompetitorManager()),
         new EditMemberType()// Eller havde vi droppet den?
         */
-<<<<<<< HEAD
-=======
-
->>>>>>> 3b3a6106d668a1c48064ee18a2430a8f5ebbba26
     };
   }
 }
