f8b96c669f59efbd4a65a4828fa3382c4527a1ee
Rasmus Kibshede <2740ralle@gmail.com>
2021-05-21 12:34:04 +0200

    CompAdm done

--- src/system/menus/competitive/commands/ShowTopFive.java
+++ src/system/menus/competitive/commands/ShowTopFive.java
@@ -1,13 +1,22 @@
 package system.menus.competitive.commands;
 
 import system.Command;
+import system.manager.Manager;
 import system.manager.ui.UI;
 
 public class ShowTopFive implements Command {
 
+  private Manager manager;
+
+  public ShowTopFive(Manager manager) {
+    this.manager = manager;
+  }
+
 
   @Override
   public void execute(UI ui) {
+
+
 
   }
 
