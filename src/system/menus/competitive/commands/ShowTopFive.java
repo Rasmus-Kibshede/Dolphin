e8b8cd743d027efd8fc3d5a67b44effc1fa2e0be
Rasmus Kibshede <2740ralle@gmail.com>
2021-05-25 12:07:10 +0200

    ShowTopFive done

--- src/system/menus/competitive/commands/ShowTopFive.java
+++ src/system/menus/competitive/commands/ShowTopFive.java
@@ -6,7 +6,9 @@ import system.manager.ui.UI;
 import system.member.competitor.Competitor;
 import system.member.competitor.Team;
 import system.member.competitor.TrainingScore;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ShowTopFive implements Command {
 
@@ -24,28 +26,35 @@ public class ShowTopFive implements Command {
       ui.displayLn("");
       ui.displayLn(team.getTeamName());
 
-      if (!team.getCompetitors().isEmpty()){
+      if (!team.getCompetitors().isEmpty()) {
         for (Competitor competitor : team.getCompetitors()) {
-          for (TrainingScore trainingScore: competitor.getTrainingScores()) {
+
+          for (TrainingScore trainingScore : competitor.getTrainingScores()) {
             if (trainingScore.getDiscipline() == team.getDiscipline()) {
               longs.add(trainingScore.getTime().toMillis());
             }
           }
         }
 
-        ArrayList<Long> top5 = new ArrayList<>(longs.subList(longs.size()-5, longs.size()));
+        Collections.sort(longs);
+        ArrayList<Long> top5;
+
+        if (longs.size() > 5){
+          top5 = new ArrayList<>(longs.subList(0, 5));
+        }else {
+          top5 = new ArrayList<>(longs.subList(0, longs.size()));
+        }
 
         for (int i = 0; i < top5.size(); i++) {
-          int RankIndex = ((i)+1);
+          int RankIndex = ((i) + 1);
           for (TrainingScore trainingScore : team.getCompetitors().get(i).getTrainingScores()) {
-            if (trainingScore.getDiscipline() == team.getDiscipline()) {
-              if (top5.get(i) == trainingScore.getTime().toMillis()) {
-                ui.display(RankIndex + ". ");
-                ui.displayLn(team.getCompetitors().get(i).getName());
-              }
+            if (top5.contains(trainingScore.getTime().toMillis())){
+              ui.display(RankIndex + ". ");
+              ui.displayLn(team.getCompetitors().get(i).getName());
             }
           }
         }
+
       } else {
         ui.displayLn("The team is empty");
       }
